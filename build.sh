6c8197dafcb152deab62ff61bb7cb5d3f4cd1642
arungeorgesaji <arungeorgesaji@gmail.com>
2024-09-09 21:52:32 +0530

    Added basic build and customize scripts(though more integration will be worked upon)

---
+++ build.sh
@@ -0,0 +1,6 @@
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+./ship --vm --name arch-vm-base --source https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-basic.qcow2
+./ship --vm exec --command "$SCRIPT_DIR/customize.sh"
+./ship --vm view arch-vm-base
