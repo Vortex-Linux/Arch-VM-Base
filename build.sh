e790fc41d7012084162cbcbc9d08256f971d7478
arungeorgesaji <arungeorgesaji@gmail.com>
2024-09-09 23:11:52 +0530

    Added logic for adding the required console to the vm in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 SCRIPT_PATH="$(readlink -f "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+XML_FILE="/tmp/arch-vm-base.xml"
 
-ship --vm --name arch-vm-base --source https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-basic.qcow2
-ship --vm exec --command "$SCRIPT_DIR/customize.sh"
+ship --vm create arch-vm-base --source https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-basic.qcow2
+
+sed -i '/<\/devices>/i \
+  <console type="pty">\
+    <target type="virtio"/>\
+  </console>' "$XML_FILE"
+
+ship --vm exec arch-vm-base --command "$SCRIPT_DIR/customize.sh"
 ship --vm view arch-vm-base
