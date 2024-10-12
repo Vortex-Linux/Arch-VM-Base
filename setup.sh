60786cbca3adf5142ee1e5d8072db4aa38d2d8b7
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-12 15:35:40 +0530

    Finished the basic installation part

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ sgdisk --new=2:0:0 --typecode=2:8e00 --change-name=2:"LVM" /dev/vda &&
 pvcreate /dev/vda2 &&
 vgcreate vg0 /dev/vda2 &&
 
-lvcreate --type thin-pool -l 1999 -n thinpool vg0 &&
+lvcreate --type thin-pool -L 1999G -n thinpool vg0 &&
 lvcreate --thin vg0/thinpool --virtualsize 10G -n swap && 
 lvcreate --thin vg0/thinpool --virtualsize 1000G -n root &&
 lvcreate --thin vg0/thinpool --virtualsize 989G -n home &&
@@ -35,6 +35,15 @@ mount /dev/vda1 /mnt/boot &&
 
 mkdir /mnt/home &&
 mount /dev/vg0/home /mnt/home &&
+
+pacman -Sy pacman-contrib &&
+
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup &&
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
+pacstrap -K /mnt base linux linux-firmware base-devel && 
+
+genfstab -U -p /mnt >> /mnt/etc/fstab 
 EOF
 )
 
