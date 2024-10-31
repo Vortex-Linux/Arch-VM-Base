b3868ec31011e1818e5ccc075e945cc08ec5bcf1
arungeorgesaji <arungeorgesaji@gmail.com>
2024-10-31 23:20:33 +0530

    Made everything work though will have to work on combining the xz and mv commands and try it out to reduce time taken to build everything

--- release.sh
+++ release.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/bash 
+
+echo "Shutting down the Arch VM..." 
+
+echo y | ship --vm shutdown arch-vm-base 
+
+echo "Optimizing the image by removing out the zeroed out blocks"
+
+ship --vm optimize arch-vm-base 
 
 echo "Starting the compression of the Arch disk image to generate the release package for 'arch-vm-base'..."
 
 DISK_IMAGE=$(sudo virsh domblklist arch-vm-base | grep .qcow2 | awk '{print $2}')
 
-xz -z "$DISK_IMAGE"
-
-mv "$DISK_IMAGE.xz" output/archlinux.qcow2.xz
+xz -c "$DISK_IMAGE" > output/archlinux.qcow2.xz
