85fa16bddc789f73c348ed0b45a56de70fa2d164
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-01 21:37:40 +0530

    Removed compression(no the optimization compression is not removed) as it was not that effective and just took way too much time

--- release.sh
+++ release.sh
@@ -8,8 +8,11 @@ echo "Optimizing the image by removing out the zeroed out blocks"
 
 ship --vm optimize arch-vm-base 
 
-echo "Starting the compression of the Arch disk image to generate the release package for 'arch-vm-base'..."
+echo "Copying the Arch disk image to generate the release package for 'arch-vm-base'..."
 
 DISK_IMAGE=$(sudo virsh domblklist arch-vm-base | grep .qcow2 | awk '{print $2}')
 
-xz -c "$DISK_IMAGE" > output/archlinux.qcow2.xz
+cp "$DISK_IMAGE" output/archlinux.qcow2
+
+echo "Copy complete. The disk image is located at output/archlinux.qcow2."
+
