a251d809122c95e9a77fcd2bbf725db9f22663ec
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-03 23:13:29 +0530

    Made a few of the changes though its still being adjusted for getting a good script for ehh v0.1.2

--- release.sh
+++ release.sh
@@ -4,15 +4,19 @@ echo "Shutting down the Arch VM..."
 
 echo y | ship --vm shutdown arch-vm-base 
 
-echo "Optimizing the image by removing out the zeroed out blocks"
+echo "Compressing the Arch VM disk image..."
 
-ship --vm optimize arch-vm-base 
+ship --vm compress arch-vm-base 
 
-echo "Copying the Arch disk image to generate the release package for 'arch-vm-base'..."
+echo "Starting the xz compression of the Arch disk image to generate the release package for 'arch-vm-base'..."
 
 DISK_IMAGE=$(sudo virsh domblklist arch-vm-base | grep .qcow2 | awk '{print $2}')
 
-cp "$DISK_IMAGE" output/archlinux.qcow2
+xz -9 -z "$DISK_IMAGE"
 
-echo "Copy complete. The disk image is located at output/archlinux.qcow2."
+echo "Moving the compressed disk image to the output directory..."
+
+mv "$DISK_IMAGE.xz" output/archlinux.qcow2.xz
+
+echo "The release package for 'arch-vm-base' has been generated successfully!"
 
